40e032b30b3a6f447a75ad21c07cf39448e5bd2e
Dhanush Shetty <dhanushshettycache@outlook.com>
2025-12-06 22:18:46 +0530

    Add description

--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,5 @@
 /*
-Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-
+Copyright © 2025 Dhanush Shetty dhanushshettycache@outlook.com
 */
 package cmd
 
@@ -10,21 +9,12 @@ import (
 	"github.com/spf13/cobra"
 )
 
-
-
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "course-flow",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Short: "A powerful CLI to manage and organize your educational courses.",
+	Long: `Course-Flow is a command-line interface (CLI) tool designed to help you efficiently manage your collection of educational courses, whether they are from online platforms, universities, or local resources.
+Use 'course-flow [command] --help' for more information about a specific command.`,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -47,5 +37,3 @@ func init() {
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
-
-
--- main.go
+++ main.go
@@ -1,6 +1,5 @@
 /*
-Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-
+Copyright © 2025 Dhanush Shetty dhanushshettycache@outlook.com
 */
 package main
 
